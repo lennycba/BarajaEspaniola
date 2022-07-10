73b7c2dece94f27dc28e67050658da579313b13d
mario <mario@LENNY>
2022-07-10 11:37:25 -0300

    Agregar archivos de proyecto.

---
+++ BarajaEspaniola/Program.cs
@@ -0,0 +1,174 @@
+﻿using BarajaEspaniola.baraja;
+
+
+////creamos una nueva baraja
+
+//var baraja = new Baraja();
+
+////mostramos la baraja ordenada
+
+//foreach (var carta in baraja.cartas)
+//{
+//    Console.WriteLine(carta.numero + " de " + carta.palo);
+//}
+
+////barajamos y mostramos la baraja mezclada
+
+//baraja.Barajar();
+//Console.WriteLine("La baraja se ha barajado");
+
+//foreach (var carta in baraja.cartas)
+//{
+//    Console.WriteLine(carta.numero + " de " + carta.palo);
+//}
+
+
+////probamos a sacar dos cartas
+
+//Console.WriteLine();
+//Console.WriteLine("--------------------------------------------------");
+//baraja.SiguienteCarta();
+
+//Console.WriteLine();
+//Console.WriteLine("--------------------------------------------------");
+//baraja.SiguienteCarta();
+
+//Console.WriteLine();
+//Console.WriteLine("--------------------------------------------------");
+
+
+////mostramos la baraja despues de sacar dos cartas
+
+//foreach (var carta in baraja.cartas)
+//{
+//    Console.WriteLine(carta.numero + " de " + carta.palo);
+//}
+
+
+
+//Console.WriteLine();
+//Console.WriteLine("-----------------repartir--------------------");
+
+////probamos a repartir 5 cartas
+
+//Console.WriteLine();
+//Console.WriteLine("----------------se repartieron---------------------");
+
+//baraja.DarCartas(5);
+
+//Console.WriteLine();
+//Console.WriteLine("-------------------------------------");
+
+//foreach (var carta in baraja.cartas)
+//{
+//    Console.WriteLine(carta.numero + " de " + carta.palo);
+//}
+
+////mostramos las cartas que ya fueron repartidas
+
+
+//Console.WriteLine();
+//Console.WriteLine("----------------se descartaron---------------------");
+
+//baraja.CartasMonton();
+//foreach (var carta in baraja.cartasDescartadas)
+//{
+//    Console.WriteLine(carta.numero + " de " + carta.palo);
+//}
+
+var baraja = new Baraja();
+int opcion = 0;
+
+Console.WriteLine("bienvenido al programa Baraja Virtual");
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("Hemos creado una nueva baraja");
+Console.WriteLine("--------------------------------------------------");
+
+while (opcion != 7)
+{
+    Console.WriteLine("--------------------------------------------------");
+    Console.WriteLine("Seleccione una de las sigientes opciones para continuar: ");
+
+    Console.WriteLine("1. Mostrar la baraja");
+    Console.WriteLine("2. Barajar");
+    Console.WriteLine("3. mostrar la carta en el tope de la baraja");
+    Console.WriteLine("4. Mostrar cuantas cartas quedan disponibles");
+    Console.WriteLine("5. Repartir cartas");
+    Console.WriteLine("6. Mostrar las cartas que ya han salido");
+    Console.WriteLine("7. Salir");
+    Console.WriteLine("--------------------------------------------------");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("A continuación se muestra la baraja");
+            Console.WriteLine("--------------------------------------------------");
+
+            baraja.MostrarBaraja();
+            break;
+
+        case 2:
+
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Se ha barajado el mazo");
+            Console.WriteLine("--------------------------------------------------");
+
+            baraja.Barajar();
+            break;
+
+        case 3:
+
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Se reparte la siguiente carta");
+            Console.WriteLine("--------------------------------------------------");
+
+            baraja.SiguienteCarta();
+            break;
+
+        case 4:
+
+            Console.Clear();
+            baraja.CartasDisponibles();
+            break;
+
+        case 5:
+
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("¿cuantas cartas desea repartir?");
+            Console.WriteLine("--------------------------------------------------");
+
+            int cantCartas = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Se reparten las siguientes " + cantCartas + " cartas");
+            Console.WriteLine("--------------------------------------------------");
+
+            baraja.DarCartas(cantCartas);
+            break;
+
+        case 6:
+
+            Console.Clear();
+            baraja.CartasMonton();
+            break;
+
+        case 7:
+
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Muchas gracias por utilizar Baraja Virtual");
+            Console.WriteLine("--------------------------------------------------");
+            break;
+    }
+}
+
+
+
---
+++ BarajaEspaniola/baraja/Carta.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarajaEspaniola.baraja
+{
+    public class Carta
+    {
+        public int numero;
+        public string palo;
+    }
+    
+    public class Baraja
+    {
+        public List<Carta> cartas;
+        public List<Carta> cartasDescartadas = new List<Carta>();
+
+        public Baraja()
+        {
+            cartas = new List<Carta>();
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    if (j == 8 || j== 9)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var carta = new Carta();
+                        carta.numero = j;
+                        switch (i)
+                        {
+                            case 1:
+                                carta.palo = "Espadas";
+                                break;
+                            case 2:
+                                carta.palo = "Copa";
+                                break;
+                            case 3:
+                                carta.palo = "Oro";
+                                break;
+                            case 4:
+                                carta.palo = "Basto";
+                                break;
+                        }
+
+                        cartas.Add(carta);
+                    }
+                    
+                }
+            }
+        }
+
+        public void Barajar()
+        {
+            Random randomizador = new Random();
+            cartas = cartas.OrderBy(x => randomizador.Next()).ToList();
+        }
+
+        
+        public void SiguienteCarta()
+        {
+            
+            cartasDescartadas.Add(cartas[0]);
+            
+            Console.WriteLine(cartas[0].numero + " de " + cartas[0].palo);
+            cartas.RemoveAt(0);
+
+            if (cartas.Count == 0)
+            {
+                Console.WriteLine("No quedan cartas en la baraja");
+            }
+        }
+        
+        public void CartasDisponibles()
+        {
+            Console.WriteLine("Aún quedan " + cartas.Count + " cartas en el mazo");
+            Console.WriteLine();
+
+        }
+
+        public void DarCartas(int numero)
+        {
+            if (cartas.Count == 0 || cartas.Count < numero)
+            {
+                Console.WriteLine("No quedan cartas en la baraja");
+            }
+            else
+            {
+                for (int i = 0; i < numero; i++)
+                {
+                    SiguienteCarta();
+                }
+            }
+
+
+        }
+
+        public void CartasMonton() 
+        {
+            
+            if (cartasDescartadas.Count == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Aún no se han descartado cartas");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Hata ahora se han descartado las siguientes "+ cartasDescartadas.Count + " cartas");
+                foreach (var carta in cartasDescartadas)
+                {
+                    Console.WriteLine(carta.numero + " de " + carta.palo);
+                }
+            }
+        }
+
+        public void MostrarBaraja()
+        {
+
+            foreach (var carta in cartas)
+            {
+                Console.WriteLine(carta.numero + " de " + carta.palo);
+            }
+        }
+    }
+
+}
